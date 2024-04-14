cbe9e5b3acf3b39ad816e0be88abc2d3eec729fc
Turlagh <turlagheoin@gmail.com>
2024-04-14 22:24:46 +0200

    Change to blockchain routing

--- node/blockchain.py
+++ node/blockchain.py
@@ -1,53 +1,26 @@
-# Python program to create Blockchain
-
-# For timestamp
 import datetime
-
-# Calculating the hash
-# in order to add digital
-# fingerprints to the blocks
 import hashlib
-
-# To store data
-# in our blockchain
 import json
-
-# For http requests to allow nodes to interact
 import requests
-
-# Flask is for creating the web
-# app and jsonify is for
-# displaying the blockchain
 from flask import Flask, jsonify, request
 
-
 class Blockchain:
-
-    # This function is created
-    # to create the very first
-    # block and set its hash to "0"
     def __init__(self):
         self.chain = []
         self.create_block(proof=1, previous_hash='0')
 
-    # This function is created
-    # to add further blocks
-    # into the chain
-    def create_block(self, proof, previous_hash):
+    def create_block(self, proof, previous_hash, data=None):
         block = {'index': len(self.chain) + 1,
-                'timestamp': str(datetime.datetime.now()),
-                'proof': proof,
-                'previous_hash': previous_hash}
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash,
+                 'data': data}  # Add 'data' field for vote
         self.chain.append(block)
         return block
 
-    # This function is created
-    # to display the previous block
     def print_previous_block(self):
         return self.chain[-1]
 
-    # This is the function for proof of work
-    # and used to successfully mine the block
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
@@ -87,39 +60,45 @@ class Blockchain:
 
         return True
 
-# Creating the Web
-# App using flask
 app = Flask(__name__)
 
-# Create the object
-# of the class blockchain
 blockchain = Blockchain()
 
-# Set to store addresses of other nodes in the network
 peer_nodes = set()
 
-# Add a new endpoint for receiving new blocks from other nodes
+def validate_block(block):
+    required_fields = ['index', 'timestamp', 'proof', 'previous_hash', 'data']
+    if not all(field in block for field in required_fields):
+        return False
+    
+    # You can add more specific validations for the 'data' field if needed
+    
+    previous_proof = block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    if proof != block['proof']:
+        return False
+    
+    previous_hash = blockchain.hash(blockchain.chain[-1])
+    if previous_hash != block['previous_hash']:
+        return False
+    
+    return True
+
 @app.route('/receive_block', methods=['POST'])
 def receive_block():
     block_data = request.get_json()
-    received_block = json.loads(block_data)
+    received_block = block_data
     
-    # Validate the received block
     if validate_block(received_block):
-        # Add the block to the local blockchain
         blockchain.chain.append(received_block)
-        
-        # Propagate the received block to other nodes in the network
         for node in peer_nodes:
             requests.post(node + '/receive_block', json=received_block)
-        
         response = {'message': 'Block added to the blockchain'}
         return jsonify(response), 200
     else:
         response = {'message': 'Invalid block received'}
         return jsonify(response), 400
 
-# Add a new endpoint for registering new nodes in the network
 @app.route('/register_node', methods=['POST'])
 def register_node():
     node_data = request.get_json()
@@ -131,36 +110,49 @@ def register_node():
     peer_nodes.add(node_address)
     return "Node registered successfully", 200
 
-# Modify the mine_block endpoint to broadcast the newly mined block
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
-    # Mine the block as before...
-    # After mining, broadcast the new block to other nodes in the network
+    last_block = blockchain.chain[-1]
+    previous_proof = last_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(last_block)
+    
+    # For simplicity, let's assume the vote data is received as a query parameter 'candidate'
+    candidate = request.args.get('candidate')
+    if not candidate:
+        return "No candidate provided", 400
+    
+    data = {'voter': request.remote_addr, 'candidate': candidate}
+    
+    block = blockchain.create_block(proof, previous_hash, data)
+    
     for node in peer_nodes:
-        requests.get(node + '/receive_block', json=blockchain.chain[-1])
+        requests.post(node + '/receive_block', json=block)
+
+    response = {'message': 'New block mined',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash'],
+                'data': block['data']}
     
     return jsonify(response), 200
 
-# Modify the display_chain endpoint to return the local blockchain
 @app.route('/get_chain', methods=['GET'])
 def display_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
-# Check validity of blockchain
-
 @app.route('/valid', methods=['GET'])
 def valid():
-	valid = blockchain.chain_valid(blockchain.chain)
+    valid = blockchain.chain_valid(blockchain.chain)
 
-	if valid:
-		response = {'message': 'The Blockchain is valid.'}
-	else:
-		response = {'message': 'The Blockchain is not valid.'}
-	return jsonify(response), 200
+    if valid:
+        response = {'message': 'The Blockchain is valid.'}
+    else:
+        response = {'message': 'The Blockchain is not valid.'}
+    return jsonify(response), 200
 
-
-# Run the flask server locally
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
